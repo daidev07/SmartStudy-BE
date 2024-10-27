1164355f54c0210da994677c850b8fdd5bfefd5a
daidev07 <hoxuandai02@gmail.com>
2024-10-27 19:33:13 +0700

    init project

---
+++ src/main/java/com/project/smartstudybejava/SmartStudyBeJavaApplication.java
@@ -0,0 +1,13 @@
+package com.project.smartstudybejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartStudyBeJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartStudyBeJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/smartstudybejava/SmartStudyBeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.smartstudybejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartStudyBeJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
