ea0e4f523d54a5ca5c15d7581730d7d4aadd9cc4
daidev07 <hoxuandai02@gmail.com>
2025-04-03 11:39:45 +0700

    fix: add bearer token for swagger

---
+++ src/main/java/com/project/smartstudybejava/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.project.smartstudybejava.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Smart Study API")
+                        .version("v1.0")
+                        .description("API Documentation for Smart Study Application"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components().addSecuritySchemes(securitySchemeName,
+                        new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
